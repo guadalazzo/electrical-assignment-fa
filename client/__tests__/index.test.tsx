be9007437b1b14102e4f6a754f65d4e92091c6f4
guadalupelazzo <guadalupelazzo@gmail.com>
2023-11-20 00:03:26 +0100

    more tests

--- client/__tests__/index.test.tsx
+++ client/__tests__/index.test.tsx
@@ -4,8 +4,14 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import Home from "../src/app/page";
 
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import ConnectorSelector from "../src/app/components/connector-selector";
+
+const mockStore = configureStore([]);
+
 describe("Home", () => {
-  it("renders ", () => {
+  it("renders select button when clicking on CCS Connector", () => {
     render(<Home />);
     const heading = screen.getByRole("heading", {
       name: "Select your connector type...",
@@ -36,5 +42,35 @@ describe("Home", () => {
       name: "Start your session",
     });
     expect(mainButton).toBeInTheDocument();
+  });
+
+  it("Show error message when nothing is selected", () => {
+    const store = mockStore({
+      connectors: {
+        connectorsList: [
+          { name: "CCS", image: "ccs", selected: false },
+          { name: "CHAdeMO", image: "chademo", selected: false },
+          { name: "AC", image: "ac", selected: false },
+        ],
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <ConnectorSelector />
+      </Provider>
+    );
+
+    expect(
+      screen.queryByText("Please select an option to continue")
+    ).toBeNull();
+
+    fireEvent.submit(
+      screen.getByRole("button", { name: "Start your session" })
+    );
+
+    expect(
+      screen.getByText("Please select an option to continue")
+    ).toBeInTheDocument();
   });
 });
